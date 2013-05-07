b805e081ab8d35a1fe032378b82df3239be46d35
cmyr <colin@cmyr.net>
2013-05-07 10:43:24 -0400

    added report view controller & basic controls

---
+++ Velobstacles/Velobstacles/VOReportViewController.h
@@ -0,0 +1,14 @@
+//
+//  VOReportViewController.h
+//  Velobstacles
+//
+//  Created by Colin Rothfels on 2013-05-07.
+//  Copyright (c) 2013 Velobstacles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VOReportViewController : UIViewController
+- (IBAction)cancelButton:(UIBarButtonItem *)sender;
+
+@end
