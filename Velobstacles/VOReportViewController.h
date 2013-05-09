3b3a471e98d6327cbb22c2f41aa5f4a07060f68d
cmyr <colin@cmyr.net>
2013-05-09 12:27:34 -0400

    switched back to static cell types for report view, implemented in-cell description editing, removed obsoleted files

--- Velobstacles/VOReportViewController.h
+++ Velobstacles/VOReportViewController.h
@@ -9,9 +9,17 @@
 #import <UIKit/UIKit.h>
 #import "VOCategoryListVC.h"
 
-@interface VOReportViewController : UITableViewController <VOCategoryTableViewDelegate>
+@interface VOReportViewController : UITableViewController <VOCategoryTableViewDelegate, UITextViewDelegate>
 
 - (IBAction)cancelAction:(UIBarButtonItem *)sender;
-- (IBAction)submitAction:(UIBarButtonItem *)sender;
-@property (weak, nonatomic) IBOutlet UIBarButtonItem *submitButton;
+//- (IBAction)submitAction:(UIBarButtonItem *)sender;
+@property (weak, nonatomic) IBOutlet UINavigationItem *navBar;
+
+// cell outlets
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *categoryLabel;
+@property (weak, nonatomic) IBOutlet UITextView *descriptionText;
+@property (weak, nonatomic) IBOutlet UILabel *photoLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
+
 @end
--- Velobstacles/VOReportViewController.m
+++ Velobstacles/VOReportViewController.m
@@ -1,4 +1,5 @@
-//
+
+ //
 //  VOReportViewController.m
 //  Velobstacles
 //
@@ -33,13 +34,20 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-//    self.report = [VOReport testReport];
     self.report = [[VOReport alloc]init];
     self.report.timestamp = [NSDate date];
     CLLocationCoordinate2D coord;
     coord.longitude = ((arc4random() % 500)*0.0001f) + -73.6;
     coord.latitude = ((arc4random() % 400)*0.0001f) + 45.49;
     self.report.location = coord;
+
+    self.descriptionText.delegate = self;
+//    NSDateFormatter* formatter = [[NSDateFormatter alloc]init];
+//    [formatter setDateStyle:NSDateFormatterMediumStyle];
+    self.dateLabel.text = [NSDateFormatter localizedStringFromDate:self.report.timestamp
+                                                         dateStyle:NSDateFormatterMediumStyle timeStyle:NSDateFormatterShortStyle];
+    
+
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
  
@@ -47,64 +55,67 @@
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 #pragma mark - Table view data source
+/*
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+//{
+////#warning Potentially incomplete method implementation.
+//    // Return the number of sections.
+//    return 2;
+//}
+//
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+//{
+////#warning Incomplete method implementation.
+//    // Return the number of rows in the section.
+//    switch (section) {
+//        case 0: return 1; break;
+//        case 1: return 3; break;
+//    }
+//    return 0;
+//}
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
-{
-//#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 2;
-}
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+//{
+//    static NSString *infoCellID = @"info";
+//    static NSString *catCellID = @"category";
+//    static NSString *descCellID = @"description";
+//    static NSString *photoCellID = @"photo";
+//
+//    NSLog(@"%@",indexPath);
+//    if (indexPath.section == 0) {
+//       VOInfoCell* cell = (VOInfoCell*)[tableView dequeueReusableCellWithIdentifier:infoCellID];
+//        cell.dateLabel.text = [self.report.timestamp descriptionWithLocale:[NSLocale currentLocale]];
+//        return cell;
+//    }else if (indexPath.section == 1 && indexPath.row == 0){
+//       VOCategoryCell* cell = (VOCategoryCell*)[tableView dequeueReusableCellWithIdentifier:catCellID];
+//        if (self.report.category) cell.textLabel.text = self.report.category;
+//        return cell;
+//    }else if (indexPath.section == 1 && indexPath.row == 1){
+//       VODescriptionCell* cell = (VODescriptionCell*)[tableView dequeueReusableCellWithIdentifier:descCellID];
+//       if (self.report.description) cell.textLabel.text = self.report.description;
+//        return cell;
+//    }else if (indexPath.section == 1 && indexPath.row == 2){
+//       VOPhotoCell* cell = (VOPhotoCell*)[tableView dequeueReusableCellWithIdentifier:photoCellID];
+//        return cell;
+//    }
+//    NSLog(@"indexPath not handled in cellForRowAtIndexPath?");
+//    return nil;
+//}
+*/
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-//#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    switch (section) {
-        case 0: return 1; break;
-        case 1: return 3; break;
-    }
-    return 0;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    static NSString *infoCellID = @"info";
-    static NSString *catCellID = @"category";
-    static NSString *descCellID = @"description";
-    static NSString *photoCellID = @"photo";
-
-    NSLog(@"%@",indexPath);
-    if (indexPath.section == 0) {
-       VOInfoCell* cell = (VOInfoCell*)[tableView dequeueReusableCellWithIdentifier:infoCellID];
-        cell.dateLabel.text = [self.report.timestamp descriptionWithLocale:[NSLocale currentLocale]];
-        return cell;
-    }else if (indexPath.section == 1 && indexPath.row == 0){
-       VOCategoryCell* cell = (VOCategoryCell*)[tableView dequeueReusableCellWithIdentifier:catCellID];
-        if (self.report.category) cell.textLabel.text = self.report.category;
-        return cell;
-    }else if (indexPath.section == 1 && indexPath.row == 1){
-       VODescriptionCell* cell = (VODescriptionCell*)[tableView dequeueReusableCellWithIdentifier:descCellID];
-       if (self.report.description) cell.textLabel.text = self.report.description;
-        return cell;
-    }else if (indexPath.section == 1 && indexPath.row == 2){
-       VOPhotoCell* cell = (VOPhotoCell*)[tableView dequeueReusableCellWithIdentifier:photoCellID];
-        return cell;
-    }
-    NSLog(@"indexPath not handled in cellForRowAtIndexPath?");
-    return nil;
-}
-
+#define DEFAULT_CELL_HEIGHT 44.0
+#define DESCRIPTION_CELL_HEIGHT 96.0
+#define PHOTO_CELL_HEIGHT 128.0
+#define CELL_PADDING 16.0
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    CGFloat cellHeight = 44.0;
-    if (indexPath.section == 1 && indexPath.row == 1) cellHeight = 96.0;
-    if (indexPath.section == 1 && indexPath.row == 2) cellHeight = 128.0;
+    CGFloat cellHeight = DEFAULT_CELL_HEIGHT;
+    if (indexPath.section == 1 && indexPath.row == 1){
+       CGFloat calculated_height = [self heightForTextView:self.descriptionText containingString:self.report.description];
+        cellHeight = DESCRIPTION_CELL_HEIGHT >  calculated_height ? DESCRIPTION_CELL_HEIGHT : calculated_height + CELL_PADDING;
+    }
+    if (indexPath.section == 1 && indexPath.row == 2) cellHeight = PHOTO_CELL_HEIGHT;
     return cellHeight;
 }
 
@@ -117,10 +128,13 @@
 {
    
     if (indexPath.section == 1 && indexPath.row == 0){
+        [self.descriptionText resignFirstResponder];
         [self performSegueWithIdentifier:CATEGORY_SEGUE sender:self];
     }else if (indexPath.section == 1 && indexPath.row == 1){
-        [self performSegueWithIdentifier:DESCRIPTION_SEGUE sender:self];
+//        [self performSegueWithIdentifier:DESCRIPTION_SEGUE sender:self];
+        [self.descriptionText becomeFirstResponder];
     }else if (indexPath.section == 1 && indexPath.row == 2){
+        [self.descriptionText resignFirstResponder];
         //handle media picking here
     }
     
@@ -136,14 +150,49 @@
     [[self presentingViewController]dismissViewControllerAnimated:YES completion:^{}];
 }
 
-- (IBAction)submitAction:(UIBarButtonItem *)sender {
-}
-- (void)viewDidUnload {
-    [self setSubmitButton:nil];
-    [super viewDidUnload];
-}
 
 #pragma mark - segues and report delegates
+
+-(void)textViewDidBeginEditing:(UITextView *)textView{
+//    show the done button, clear the textview if our report text is blank;
+    if ([self.report.description isEqualToString:@""] || !self.report.description) self.descriptionText.text = @"";
+    self.navBar.rightBarButtonItem = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemDone target:self action:@selector(descriptionEditingFinished)];
+}
+
+//text view delegate methods:
+- (CGFloat)heightForTextView:(UITextView*)textView containingString:(NSString*)string
+{
+    //helping with resizing our description text field as the user enters input
+    // simple solution courtesy of http://www.howlin-interactive.com/2013/01/creating-a-self-sizing-uitextview-within-a-uitableviewcell-in-ios-6/
+    if (string.length > 0){
+        if ([[string substringFromIndex:(string.length -1)] isEqualToString:@"\n"]) {
+            string = [string stringByAppendingString:@"B"];
+        }
+    }
+    float horizontalPadding = 16;
+    float verticalPadding = 16;
+    float widthOfTextView = textView.contentSize.width - horizontalPadding;
+    float height = [string sizeWithFont:[UIFont systemFontOfSize:14.0] constrainedToSize:CGSizeMake(widthOfTextView, 999999.0f) lineBreakMode:NSLineBreakByWordWrapping].height + verticalPadding;
+    
+    return height;
+}
+
+-(void)textViewDidChange:(UITextView *)textView{
+    //recalculate height of textview as it is edited;
+    self.report.description = textView.text;
+    [self.tableView beginUpdates];
+    [self.tableView endUpdates];
+    
+}
+-(void)descriptionEditingFinished{
+    [self.descriptionText resignFirstResponder];
+    if ([self.descriptionText.text isEqualToString:@""]){
+        self.descriptionText.text = @"Description";
+    }else{
+        self.report.description = self.descriptionText.text;
+    }
+    [self.navBar setRightBarButtonItem:nil animated:NO];
+}
 
 
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
@@ -155,6 +204,18 @@
 
 -(void)categoryRecieved:(NSString *)category{
     self.report.category = category;
-    [self.tableView reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:0 inSection:1]] withRowAnimation:UITableViewRowAnimationAutomatic];
+    self.categoryLabel.text = category;
+//    [self.tableView reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:0 inSection:1]] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
+
+- (void)viewDidUnload {
+    [self setDateLabel:nil];
+    [self setCategoryLabel:nil];
+    [self setPhotoLabel:nil];
+    [self setPhotoImageView:nil];
+    [self setDescriptionText:nil];
+    [self setNavBar:nil];
+    [super viewDidUnload];
+}
+
 @end
